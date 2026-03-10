7c1be7d3eec2cac1397c64a912b27bc383b7ebff
Mirae <mirae2thefuture@gmail.com>
2026-03-10 18:52:34 +0900

    refactor: Models/Domain/Data 구조 정리

---
+++ LottoMate/LottoMate/Sources/Domain/Lottery/LotteryType.swift
@@ -0,0 +1,14 @@
+//
+//  LotteryType.swift
+//  LottoMate
+//
+//  Created by Mirae on 3/10/26.
+//
+
+import Foundation
+
+enum LotteryType: String {
+    case lotto = "로또"
+    case pensionLottery = "연금복권"
+    case speeto = "스피또"
+}
--- LottoMate/LottoMate/Sources/Features/WinningInfoDetail/SubViews/SpeetoWinningInfoDetail/SpeetoWinningInfoReactor.swift
+++ LottoMate/LottoMate/Sources/Features/WinningInfoDetail/SubViews/SpeetoWinningInfoDetail/SpeetoWinningInfoReactor.swift
@@ -10,33 +10,6 @@ import ReactorKit
 import RxSwift
 import RxCocoa
 
-enum SpeetoType: String {
-    case s500 = "S500"  // API 값을 rawValue로 사용
-    case s1000 = "S1000"
-    case s2000 = "S2000"
-    
-    var displayName: String {
-        switch self {
-        case .s500: return "스피또 500"
-        case .s1000: return "스피또 1000"
-        case .s2000: return "스피또 2000"
-        }
-    }
-    
-    var buttonText: String {
-        switch self {
-        case .s500: return "500"
-        case .s1000: return "1000"
-        case .s2000: return "2000"
-        }
-    }
-    
-    // the500 등 기존 값으로도 접근 가능하도록 정적 프로퍼티 제공 (필요한 경우)
-    static let the500 = SpeetoType.s500
-    static let the1000 = SpeetoType.s1000
-    static let the2000 = SpeetoType.s2000
-}
-
 final class SpeetoWinningInfoReactor: Reactor {
     
     enum Action {
