bfbb2439db73dce9dd9b9b6d38f80857d0d77ff9
Mirae <mirae2thefuture@gmail.com>
2026-03-18 15:45:49 +0900

    refactor: WinningInfoDetail 진입 상태 모델 추가

---
+++ LottoMate/LottoMate/Sources/Features/WinningInfoDetail/WinningInfoDetailState.swift
@@ -0,0 +1,33 @@
+//
+//  WinningInfoDetailState.swift
+//  LottoMate
+//
+//  Created by Mirae on 3/18/26.
+//
+
+import Foundation
+
+struct WinningInfoDetailState: Equatable {
+    var selectedLotteryType: LotteryType
+    var latestLotteryResult: LatestLotteryWinningInfoModel?
+    var lottoRoundResult: LottoResultModel?
+    var pensionRoundResult: PensionLotteryResultModel?
+    var currentLottoRound: Int?
+    var currentPensionLotteryRound: Int?
+
+    init(
+        selectedLotteryType: LotteryType,
+        latestLotteryResult: LatestLotteryWinningInfoModel? = nil,
+        lottoRoundResult: LottoResultModel? = nil,
+        pensionRoundResult: PensionLotteryResultModel? = nil,
+        currentLottoRound: Int? = nil,
+        currentPensionLotteryRound: Int? = nil
+    ) {
+        self.selectedLotteryType = selectedLotteryType
+        self.latestLotteryResult = latestLotteryResult
+        self.lottoRoundResult = lottoRoundResult
+        self.pensionRoundResult = pensionRoundResult
+        self.currentLottoRound = currentLottoRound
+        self.currentPensionLotteryRound = currentPensionLotteryRound
+    }
+}
